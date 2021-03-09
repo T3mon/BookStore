4f9f2042cfc6530ddbc0004ca314cbd8b7a703c6
student <44963781+T3mon@users.noreply.github.com>
2021-03-09 15:48:53 +0200

    Create user model

---
+++ BookStore/Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+
+    }
+}
